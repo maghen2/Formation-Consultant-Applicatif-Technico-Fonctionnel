3007ad53dfec11cca966836ef1ced8a3a958b0a8
maghen2 <maghen2@gmail.com>
2024-06-21 16:34:47 +0200

    TP_Algo_Python_en_C

---
+++ C-C++/TP_Algo_Python_en_C_maghen.c
@@ -0,0 +1,8 @@
+﻿// TP_Algo_Python_en_C
+// TP : Gestion de document et analyse logique (Jour 1)
+// Rostand MAGHEN
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
