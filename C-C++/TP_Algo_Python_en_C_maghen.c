4cccc9476c2d32bf24a5bead3aea8458a38a30a9
maghen2 <maghen2@gmail.com>
2024-06-21 16:45:23 +0200

    main()

--- C-C++/TP_Algo_Python_en_C_maghen.c
+++ C-C++/TP_Algo_Python_en_C_maghen.c
@@ -29,3 +29,16 @@ int tableauDivisible(int tableau[MAX_TAB_SIZE]);
 void tableauCroissant(int tableau[MAX_TAB_SIZE]);
 void tableauDecroissant(int tableau[MAX_TAB_SIZE]);
 int tableauCumule(int tableau[MAX_TAB_SIZE]);
+
+// 3.	Écrire un programme qui prend en entrée des nombres et retourne un tableau ordonné.  Exemple :  J’entre      1 4 2 7 12 4 9   Il retourne       	1 2 4 4 7 9 12
+int sainsiesaisieInt2Array();
+// 7.	Écrire un algorithme qui prend en entrée le résultat de l’exercice 6 et un autre nombre que fourni l’utilisateur. Celui-ci sélectionne tous les éléments qui sont dans la même dizaine que celui entrée par l’utilisateur en donnant aussi leur position dans le tableau.
+void tableauMemeDizaine(int choix, int tableau[MATRICE_TAB_SIZE][MATRICE_TAB_SIZE]);
+
+// Declarartion des variables globales
+int nombreDeEntiers=0;
+
+int main() {
+saisieInt2Array();
+   return 0;
+}
